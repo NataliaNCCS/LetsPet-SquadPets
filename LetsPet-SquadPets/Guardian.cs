13a9d27bdf5a469d258c28ed95a84bcb208258c3
Natália Campos <nataliacamposnccs@gmail.com>
2022-06-21 17:35:51 -0300

    Add project files.

---
+++ LetsPet-SquadPets/Guardian.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsPet_SquadPets
+{
+    public class Guardian : Person
+    {
+        public DateTime RegistrationDate { get; set; }
+        public List<Animal> PetList { get; set; }
+
+
+        // MÉTODOS
+        public void PrintGuardian()
+        {
+            Console.WriteLine("IMPRIMINDO TUTOR:");
+            Console.WriteLine($"Nome: {Name}");
+            Console.WriteLine($"CPF: {CPF}");
+            Console.WriteLine($"WhatsApp: {WhatsApp}");
+            Console.WriteLine($"Telefone Fixo: {PhoneNumber}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"CEP: {CEP}");
+            Console.WriteLine($"Endereço: {StreetName}, {HouseNumber}");
+            Console.WriteLine($"Complemento: {AdditionalAddress}");
+            Console.WriteLine("\nPETS DO TUTOR:");
+            PrintPetList();
+        }
+
+        public void PrintPetList()
+        {
+            foreach(Animal pet in PetList)
+                Console.WriteLine(pet);
+        }
+
+        public void AddPet(Animal pet)
+        {
+            PetList.Add(pet);
+        }
+
+    }
+}
---
+++ LetsPet-SquadPets/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsPet_SquadPets
+{
+    public class Person
+    {
+        public string Name { get; set; }    
+        public string CPF { get; set; }
+        public string WhatsApp { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string CEP { get; set; }
+        public int HouseNumber { get; set; } 
+        public string StreetName { get; set; }
+        public string AdditionalAddress { get; set; }
+
+
+    }
+}
