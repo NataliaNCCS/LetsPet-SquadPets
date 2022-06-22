cda0a77b29a35ddbdea74bc42dd407d68c46adb7
Natália Campos <nataliacamposnccs@gmail.com>
2022-06-22 17:56:05 -0300

    Commit nat campos Vacina

--- LetsPet-SquadPets/VaccineSystemRegister.cs
+++ LetsPet-SquadPets/VaccineSystemRegister.cs
@@ -9,21 +9,72 @@ namespace LetsPet_SquadPets
     public class VaccineSystemRegister
     {
         public string VaccineName { get; set; }
-        public Species SpeciesVaccine { get; set; }
+        public Species VaccineSpecies { get; set; }
         public int VaccineValidity { get; set; }
 
-        public List<VaccineRegister> ListVaccine { get; set; }
+        public List<VaccineSystemRegister> ListVaccine { get; set; }
 
-        public VaccineSystemRegister(string vaccineName, Species speciesVaccine, int vaccineValidity)
+        public Species GetVaccineSpecies()
         {
-            VaccineName = vaccineName;
-            SpeciesVaccine = speciesVaccine;
-            VaccineValidity = vaccineValidity;
+
+            bool valido = true;
+            int inputNumerico;
+            Species vaccineSpecies;
+            do
+            {
+                Console.WriteLine("Para qual espécie é a vacina a ser cadastrada? (Digite 1 para cachorro ou 2 para gato)");
+                if (!valido)
+                {
+                    Console.WriteLine("Erro na leitura, digite uma das especies acima: ");
+                }
+                var input = Console.ReadLine();
+                valido = int.TryParse(input, out inputNumerico) && (inputNumerico == 1 || inputNumerico == 2);
+            } while (!valido);
+            vaccineSpecies = (Species)inputNumerico;
+            return vaccineSpecies;
         }
 
-        public void AddVaccineRegister(VaccineRegister vaccine)
+        public string GetVaccineName()
         {
-            ListVaccine.Add(vaccine);
+            string vaccineName;
+            do
+            {
+                Console.WriteLine("Insira o nome da Vacina a ser cadastrada:");
+                vaccineName = Console.ReadLine();
+                if (!string.IsNullOrEmpty(vaccineName))
+                {
+                    break;
+                }
+                Console.WriteLine("O valor não pode ser vazio.");
+            } while (true);
+            return vaccineName;
+        }
+        public int GetVaccineValidity()
+        {
+            int vaccineValidity;
+            do
+            {
+                Console.WriteLine("Qual a validade em meses da vacina? ");
+                if (int.TryParse(Console.ReadLine(), out vaccineValidity))
+                {
+                    if (vaccineValidity > 0)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("Valor inválido.");
+            } while (true);
+            return vaccineValidity;
+        }
+
+
+        public void AddListVaccine(VaccineSystemRegister newVaccine)
+        {
+            newVaccine.VaccineName = GetVaccineName();
+            newVaccine.VaccineSpecies = GetVaccineSpecies();
+            newVaccine.VaccineValidity = GetVaccineValidity();
+
+            ListVaccine.Add(newVaccine);
         }
 
     }
